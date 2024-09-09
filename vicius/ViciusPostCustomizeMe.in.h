03dc5dd581ecbcc6bc7b003f475019ada21b7d9e
Fred Emmott <git@fredemmott.com>
2024-09-09 12:04:40 -0500

    Fix filename regex

--- vicius/ViciusPostCustomizeMe.in.h
+++ vicius/ViciusPostCustomizeMe.in.h
@@ -5,7 +5,7 @@
 
 // Allow hyphens in particular
 #undef NV_FILENAME_REGEX
-#define NV_FILENAME_REGEX R"((\\w+)_([a-zA-Z0-9-]+)_Updater)"
+#define NV_FILENAME_REGEX R"((\w+)_([a-zA-Z0-9-]+)_Updater)"
 
 #undef NV_CUSTOM_VERSION_SUFFIX
 #define NV_CUSTOM_VERSION_SUFFIX "@VERSION_SUFFIX@"
